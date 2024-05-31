7d7ad74dc0581e9dcb7c59f198ec56b28e2fe659
John Gorski <john.gorski.jr@gmail.com>
2024-05-31 10:20:21 -0700

    Replace demo sketch with Asteroids ship lookalike

--- src/quilsteroids/core.clj
+++ src/quilsteroids/core.clj
@@ -1,48 +1,96 @@
 (ns quilsteroids.core
-  (:require [quil.core :as q]
-            [quil.middleware :as m]))
+  (:require
+   [quil.core :as q]
+   [quil.middleware :as m]))
 
 (defn setup []
-  ; Set frame rate to 30 frames per second.
+  ;; Set frame rate to 30 frames per second.
   (q/frame-rate 30)
-  ; Set color mode to HSB (HSV) instead of default RGB.
+  ;; Set color mode to HSB (HSV) instead of default RGB.
   (q/color-mode :hsb)
-  ; setup function returns initial state. It contains
-  ; circle color and position.
-  {:color 0
-   :angle 0})
+  ;; setup function returns initial state. It contains
+  {:angle 0})
 
 (defn update-state [state]
-  ; Update sketch state by changing circle color and position.
-  {:color (mod (+ (:color state) 0.7) 255)
-   :angle (+ (:angle state) 0.1)})
+  (assoc state :angle (+ (:angle state) (/ Math/PI 20))))
 
-(defn draw-state [state]
-  ; Clear the sketch by filling it with light-grey color.
-  (q/background 240)
-  ; Set circle color.
-  (q/fill (:color state) 255 255)
-  ; Calculate x and y coordinates of the circle.
-  (let [angle (:angle state)
-        x (* 150 (q/cos angle))
-        y (* 150 (q/sin angle))]
-    ; Move origin point to the center of the sketch.
-    (q/with-translation [(/ (q/width) 2)
-                         (/ (q/height) 2)]
-      ; Draw the circle.
-      (q/ellipse x y 100 100))))
+(defn vec2d+ [a b]
+  (mapv + a b))
 
+(defn rectangular [r theta]
+  [(* r (Math/cos theta)) (* r (Math/sin theta))])
 
-(q/defsketch quilsteroids
-  :title "You spin my circle right round"
-  :size [500 500]
-  ; setup function called only once, during sketch initialization.
-  :setup setup
-  ; update-state is called on each iteration before draw-state.
-  :update update-state
-  :draw draw-state
-  :features [:keep-on-top]
-  ; This sketch uses functional-mode middleware.
-  ; Check quil wiki for more info about middlewares and particularly
-  ; fun-mode.
-  :middleware [m/fun-mode])
+(defn draw-ship [{:keys [x y theta thrusting?]}]
+  (let [R 10
+        r 7
+        delta (* 5/6 Math/PI)
+        left-angle (+ theta delta)
+        right-angle (- theta delta)
+        pos [x y]
+        nose (vec2d+ pos (rectangular R theta))
+        left-tip (vec2d+ pos (rectangular R left-angle))
+        left-base (vec2d+ pos (rectangular r left-angle))
+        right-tip (vec2d+ pos (rectangular R right-angle))
+        right-base (vec2d+ pos (rectangular r right-angle))
+        tail (vec2d+ pos (rectangular R (+ theta Math/PI)))]
+    (q/stroke 200)
+    (q/line nose left-tip)
+    (q/line nose right-tip)
+    (q/line left-base right-base)
+    (when thrusting?
+      (q/line left-base tail)
+      (q/line right-base tail))))
+
+(defn draw-state [{:keys [angle color thrusting?] :as state}]
+  (q/background 0)
+  ;; Move origin point to the center of the sketch.
+  (q/with-translation [(/ (q/width) 2)
+                       (/ (q/height) 2)]
+    (draw-ship {:x 0 :y 0 :theta angle :thrusting? thrusting?})))
+
+(defn key-pressed [old-state event]
+  (cond
+    (= :up (:key event))
+    (assoc old-state :thrusting? true)
+
+    (= :right (:key event))
+    (assoc old-state :right-turn? true)
+
+    (= :left (:key event))
+    (assoc old-state :left-turn? true)
+
+    :default
+    old-state))
+
+(defn key-released [old-state event]
+  (cond
+    (= :up (:key event))
+    (dissoc old-state :thrusting?)
+
+    (= :right (:key event))
+    (dissoc old-state :right-turn?)
+
+    (= :left (:key event))
+    (dissoc old-state :left-turn?)
+
+    :default
+    old-state))
+
+(comment
+  (q/defsketch quilsteroids
+    :title "Quilsteroids!"
+    :size [500 500]
+    ;; setup function called only once, during sketch initialization.
+    :setup setup
+    ;; update-state is called on each iteration before draw-state.
+    :update update-state
+    :draw draw-state
+
+    :key-pressed key-pressed
+    :key-released key-released
+
+    :features [:keep-on-top]
+    ;; This sketch uses functional-mode middleware.
+    ;; Check quil wiki for more info about middlewares and particularly
+    ;; fun-mode.
+    :middleware [m/fun-mode]))
