2ea501329bef7041eb243c7b8e21225e837764cf
John Gorski <john.gorski.jr@gmail.com>
2024-06-01 12:39:25 -0700

    use with-translation to refactor ship drawing

--- src/quilsteroids/core.clj
+++ src/quilsteroids/core.clj
@@ -5,15 +5,17 @@
    [quil.middleware :as m])
   (:import (clojure.lang PersistentQueue)))
 
+(def play-area
+  [640 480])
+
 (defn setup []
-  ;; Set frame rate to 30 frames per second.
   (q/frame-rate 30)
   {:controls #{}
    :events PersistentQueue/EMPTY
 
    :ship
    {:angle 0
-    :position [0 0]
+    :position (mapv #(/ % 2) play-area)
     :velocity [0 0]
     :ammo 4}
    })
@@ -33,17 +35,17 @@
   )
 
 (def turn-rate (/ Math/PI 20))
+(def ship-acceleration 1/4)
 
 (defn actuate-controls [{:keys [controls ship] :as state}]
   (-> state
       (update-in [:ship :angle]
-                 #(+ %
-                     (* turn-rate
-                        (+ (if (:left-turn controls) -1 0)
-                           (if (:right-turn controls) 1 0)))))
+                 #(+ % (* turn-rate
+                          (+ (if (:left-turn controls) -1 0)
+                             (if (:right-turn controls) 1 0)))))
       (update-in [:ship :velocity]
                  #(v+ % (rectangular
-                         (if (:thrusters controls) 1/4 0)
+                         (* ship-acceleration (if (:thrusters controls) 1 0))
                          (:angle ship))))))
 
 (defn move-ship [ship]
@@ -64,31 +66,29 @@
   [(* r (Math/cos theta)) (* r (Math/sin theta))])
 
 (defn draw-ship [{:keys [position angle thrusting?]}]
-  (let [R 10
-        r 7
-        delta (* 5/6 Math/PI)
-        left-angle (+ angle delta)
-        right-angle (- angle delta)
-        nose (v+ position (rectangular R angle))
-        left-tip (v+ position (rectangular R left-angle))
-        left-base (v+ position (rectangular r left-angle))
-        right-tip (v+ position (rectangular R right-angle))
-        right-base (v+ position (rectangular r right-angle))
-        tail (v+ position (rectangular R (+ angle Math/PI)))]
-    (q/stroke 200)
-    (q/line nose left-tip)
-    (q/line nose right-tip)
-    (q/line left-base right-base)
-    (when thrusting?
-      (q/line left-base tail)
-      (q/line right-base tail))))
+  (q/with-translation position
+    (let [R 10
+          r 7
+          delta (* 5/6 Math/PI)
+          left-angle (+ angle delta)
+          right-angle (- angle delta)
+          nose (rectangular R angle)
+          left-tip (rectangular R left-angle)
+          left-base (rectangular r left-angle)
+          right-tip (rectangular R right-angle)
+          right-base (rectangular r right-angle)
+          tail (rectangular R (+ angle Math/PI))]
+      (q/stroke 200)
+      (q/line nose left-tip)
+      (q/line nose right-tip)
+      (q/line left-base right-base)
+      (when thrusting?
+        (q/line left-base tail)
+        (q/line right-base tail)))))
 
 (defn draw-state [{:keys [controls ship] :as state}]
   (q/background 0)
-  ;; Move origin point to the center of the sketch.
-  (q/with-translation [(/ (q/width) 2)
-                       (/ (q/height) 2)]
-    (draw-ship (assoc ship :thrusting? (:thrusters controls)))))
+  (draw-ship (assoc ship :thrusting? (:thrusters controls))))
 
 (def held-controls
   {:up :thrusters
@@ -117,7 +117,7 @@
     :middleware [m/fun-mode]
     :features [:keep-on-top]
     :title "Quilsteroids!"
-    :size [640 480]
+    :size play-area
     ;; setup function called only once, during sketch initialization.
     :setup setup
     ;; update-state is called on each iteration before draw-state.
